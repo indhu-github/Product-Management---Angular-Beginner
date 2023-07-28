c7b67d9acc03d179b5d8d407961195a87cca3c24
indhu yelineni <indhusworkmail@gmail.com>
2023-07-28 12:05:35 -0400

    Bug fixes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,12 +6,24 @@ import { IProduct } from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter', value);
+    this.filteredProducts = this.performFilter(value);
+    console.log(this.filteredProducts);
+  }
+
+  filteredProducts: IProduct[] = [];
   products: IProduct[] = [
     {
       productId: 1,
@@ -33,13 +45,50 @@ export class ProductListComponent implements OnInit{
       starRating: 4.2,
       imageUrl: 'assets/images/garden_cart.png',
     },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2019',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+    {
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2019',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      starRating: 3.7,
+      imageUrl: 'assets/images/saw.png',
+    },
+    {
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2018',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      starRating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.png',
+    },
   ];
 
   ngOnInit(): void {
-   console.log('In OnInit');
+    this._listFilter = 'am';
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => 
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
   }
 }
