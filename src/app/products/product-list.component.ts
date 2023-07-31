466e4f2c5320043d0ace643c1343261ab25cc542
indhu yelineni <indhusworkmail@gmail.com>
2023-07-31 17:12:35 -0400

    Emitting an event

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -91,4 +91,8 @@ export class ProductListComponent implements OnInit {
       product.productName.toLocaleLowerCase().includes(filterBy)
     );
   }
+
+  onRatingClicked(message:string){
+    this.pageTitle = 'Product List:' + message;
+  }
 }
