d114ad80f0bf5b887a0a275ea0832e91486b304f
indhu yelineni <indhusworkmail@gmail.com>
2023-08-08 15:46:18 -0400

    Created product module and shared module to organize the application files properly

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,31 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { ConvertToSpaces } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+import { ConvertToSpaces } from '../shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpaces,
-    StarComponent
+    ConvertToSpaces
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path:'products',component:ProductListComponent},
       {path:'products/:id',
       canActivate:[ProductDetailGuard],
       component:ProductDetailComponent},
-      ]) // forChild() method is used since 
-    //it is a feature module and routes available to the
-    //child modules
+      ]),  // forChild() method is used since 
+      //it is a feature module and routes available to the
+      //child modules
+    SharedModule
   ]
 })
 export class ProductModule { }
